fe70fafbd2f4074aa42c2a3cb408ff7c81738a99
riccardogiacomazzi <riccardogiacomazzi93@gmail.com>
2023-12-04 15:07:41 +0100

    new production build committed

--- index.js
+++ index.js
@@ -100,7 +100,7 @@ app.put("/api/persons/:id", (request, response) => {
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
